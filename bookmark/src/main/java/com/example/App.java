8a3ccabcb8783863d3241fa45058afb921e06ffa
Toshiaki Maki <makingx@gmail.com>
2014-07-26 15:28:33 +0900

    Separated configuration

--- bookmark/src/main/java/com/example/App.java
+++ bookmark/src/main/java/com/example/App.java
@@ -2,64 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @EnableAutoConfiguration
 @ComponentScan
-@Configuration
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    /**
-     * http://spring.io/guides/gs/rest-service-cors/
-     *
-     * @return CORSFilter
-     */
-    @Bean
-    FilterRegistrationBean corsFilter() {
-        return new FilterRegistrationBean(new Filter() {
-            public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-                    throws IOException, ServletException {
-                HttpServletRequest request = (HttpServletRequest) req;
-                HttpServletResponse response = (HttpServletResponse) res;
-                String method = request.getMethod();
-                response.setHeader("Access-Control-Allow-Origin", "*");
-                response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
-                response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
-                response.setHeader("Access-Control-Allow-Credentials", "true");
-                response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
-                if ("OPTIONS".equals(method)) {
-                    response.setStatus(HttpStatus.OK.value());
-                } else {
-                    chain.doFilter(req, res);
-                }
-            }
-
-            public void init(FilterConfig filterConfig) {
-            }
-
-            public void destroy() {
-            }
-        });
-    }
-
-    @Bean
-    CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        return filter;
     }
 }
